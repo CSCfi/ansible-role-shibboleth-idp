5e9ae7ee5d2b2571607251de8a17aec698aabe45
Sami Silén <silen@laukkoski.com>
2021-02-08 11:57:19 +0200

    Fixed fixed values to variables

--- templates/opt/shibboleth-idp/bin/letsencrypt-hook.sh
+++ templates/opt/shibboleth-idp/bin/letsencrypt-hook.sh
@@ -2,25 +2,25 @@
 
 set -e
 
-            pkpass={{ shibbolethidp_keystore_password }}
-            storepass={{ shibbolethidp_keystore_password }}
-            keystore={{ shibbolethidp_idp_home }}/credentials/ssl-keystore.jks
+pkpass={{ shibbolethidp_keystore_password }}
+storepass={{ shibbolethidp_keystore_password }}
+keystore={{ shibbolethidp_idp_home }}/credentials/ssl-keystore.jks
 
-            umask 077
+umask 077
 
-            cd "$RENEWED_LINEAGE"
+[ -z "$RENEWED_LINEAGE" ] && cd /etc/letsencrypt/live/{{ shibbolethidp_fqdn }} || cd "$RENEWED_LINEAGE"
 
-            openssl pkcs12 -export \
-             -inkey privkey.pem -in fullchain.pem \
-             -out ssl-keystore.pkcs12 -passout "pass:$pkpass"
+openssl pkcs12 -export \
+	-inkey privkey.pem -in fullchain.pem \
+	-out ssl-keystore.pkcs12 -passout "pass:$pkpass"
 
-            keytool -importkeystore -noprompt \
-             -srckeystore ssl-keystore.pkcs12 -srcstoretype PKCS12 -srcstorepass "$pkpass" \
-             -destkeystore ssl-keystore.jks -deststorepass "$storepass"
+keytool -importkeystore -noprompt \
+	-srckeystore ssl-keystore.pkcs12 -srcstoretype PKCS12 -srcstorepass "$pkpass" \
+	-destkeystore ssl-keystore.jks -deststorepass "$storepass"
 
-            chown jetty:jetty ssl-keystore.jks
-            chmod 440 ssl-keystore.jks
-	    mv "$keystore" "$(date +%Y%m%d)-$keystore"
-            mv ssl-keystore.jks "$keystore"
+chown jetty:jetty ssl-keystore.jks
+chmod 550 ssl-keystore.jks
+[ -f $keystore ] && mv "$keystore" "$(dirname "${keystore}")/$(date +%Y%m%d)-$(basename "${keystore}")"
+mv ssl-keystore.jks "$keystore"
 
-            service jetty restart
+systemctl restart shibboleth-idp
